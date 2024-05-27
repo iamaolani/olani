49645cf8951b3149a3dead7020e42a7ecde5147e
iamao <164877307+iamaolani@users.noreply.github.com>
2024-05-28 05:51:02 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-d3d3c89e-b0a5-44c9-9d3d-501d5e247ab5/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-ad748a28-04ba-4715-9a65-df384fbe339e/)
 
 
 echo "$curl_output" >> Update.tsx
